0449e29682ce88ae79a3c61a5ae3cdef2982723b
davidivad96 <david@strivelabs.io>
2022-01-08 23:02:33 +0100

    Add title to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { ConnectWallet } from '@3rdweb/react';
-import { Center } from '@chakra-ui/react';
+import { Center, Text, VStack } from '@chakra-ui/react';
 
-const App = () => (
+const App: React.FunctionComponent = () => (
   <Center bgGradient="linear(to-r, #1A2980, #26D0CE)" h="100vh" color="white">
-    <ConnectWallet size="lg" colorScheme="blackAlpha" />
+    <VStack spacing={5}>
+      <Text fontSize="6xl">Welcome to Web3Chat!</Text>
+      <ConnectWallet size="lg" colorScheme="blackAlpha" />
+    </VStack>
   </Center>
 );
 export default App;
