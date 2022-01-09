565adf553d7196190c6db430fdeaffb8ffe97f6c
davidivad96 <david@strivelabs.io>
2022-01-09 17:08:20 +0100

    Init and configure AWS Amplify

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,10 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { ThirdwebProvider } from '@3rdweb/react';
 import { ConnectorOptions } from '@3rdweb/hooks';
 import { BrowserRouter } from 'react-router-dom';
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 const supportedChainIds = [1, 4, 137, 250, 43114, 80001];
 const connectors: Partial<ConnectorOptions> = {
