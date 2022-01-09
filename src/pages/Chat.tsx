2706deaba658de33d6b2778c8208e448142d975a
davidivad96 <david@strivelabs.io>
2022-01-09 14:16:06 +0100

    Implement Chat and Trivia pages

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,3 +1,13 @@
-const Chat: React.FunctionComponent = () => <p>Chat page</p>;
+import { Box, Container, Text } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+
+const Chat: React.FunctionComponent = () => (
+  <Container maxW="container.xl" h="full" py="4">
+    <Box bg="#ededed" h="full" borderRadius={3}>
+      <Navbar />
+      <Text>CHAT</Text>
+    </Box>
+  </Container>
+);
 
 export default Chat;
--- src/pages/Trivia.tsx
+++ src/pages/Trivia.tsx
@@ -1,3 +1,13 @@
-const Trivia: React.FunctionComponent = () => <p>Trivia page</p>;
+import { Box, Container, Text } from '@chakra-ui/react';
+import Navbar from '../components/Navbar';
+
+const Trivia: React.FunctionComponent = () => (
+  <Container maxW="container.xl" h="full" py="4">
+    <Box bg="#ededed" h="full" borderRadius={3}>
+      <Navbar />
+      <Text>TRIVIA</Text>
+    </Box>
+  </Container>
+);
 
 export default Trivia;
