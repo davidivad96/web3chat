496bcbf52d84e9c2ef6962cc99cc1f6c09748c53
davidivad96 <david@strivelabs.io>
2022-01-09 14:15:49 +0100

    Implement Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,82 @@
+import { Link as ReachLink } from 'react-router-dom';
+import {
+  Avatar,
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  Link,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Stack,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+const Links = [
+  { title: 'Chat', to: '/chat' },
+  { title: 'Trivia', to: '/trivia' },
+];
+
+const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems="center" justifyContent="space-between">
+        <IconButton
+          size="md"
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label="Open Menu"
+          display={{ md: 'none' }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack spacing={8} alignItems="center">
+          <Box>Logo</Box>
+          <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
+            {Links.map((link) => (
+              <Link key={link.title} as={ReachLink} to={link.to}>
+                {link.title}
+              </Link>
+            ))}
+          </HStack>
+        </HStack>
+        <Flex alignItems="center">
+          <Menu>
+            <MenuButton rounded="full" variant="link" cursor="pointer" minW={0}>
+              <Avatar
+                size="sm"
+                src={
+                  'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                }
+              />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Link 1</MenuItem>
+              <MenuItem>Link 2</MenuItem>
+              <MenuDivider />
+              <MenuItem>Link 3</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
+      {isOpen && (
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack spacing={4}>
+            {Links.map((link) => (
+              <Link key={link.title} as={ReachLink} to={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </Stack>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default Navbar;
