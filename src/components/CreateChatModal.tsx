0fcccba869cad76968b0199892756e0c9bc5d066
davidivad96 <davidivad96@gmail.com>
2022-01-12 21:31:02 +0100

    Implement Create new chat functionality

--- src/components/CreateChatModal.tsx
+++ src/components/CreateChatModal.tsx
@@ -3,6 +3,7 @@ import {
   Avatar,
   Button,
   Center,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -17,7 +18,8 @@ import ClipLoader from 'react-spinners/ClipLoader';
 import { API, graphqlOperation } from 'aws-amplify';
 import { GraphQLResult } from '@aws-amplify/api';
 import { listAccounts } from '../graphql/queries';
-import { ListAccountsQuery } from '../API';
+import { createAccountChat, createChat } from '../graphql/mutations';
+import { CreateChatMutation, ListAccountsQuery } from '../API';
 import { Account } from '../interfaces';
 import { AccountContext } from '../contexts/Account';
 
@@ -32,7 +34,9 @@ const CreateChatModal: React.FunctionComponent<Props> = ({ isOpen, onClose }) =>
   } = useContext(AccountContext);
   const [accounts, setAccounts] = useState<Account[]>([]);
   const [selected, setSelected] = useState<string[]>([]);
+  const [chatName, setChatName] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
 
   const updateSelected = useCallback((address: string) => {
     setSelected((selected) =>
@@ -40,10 +44,14 @@ const CreateChatModal: React.FunctionComponent<Props> = ({ isOpen, onClose }) =>
     );
   }, []);
 
+  const updateChatName = useCallback((evt: React.ChangeEvent<HTMLInputElement>) => setChatName(evt.target.value), []);
+
   const isSelected = useCallback((address: string | undefined) => address && selected.includes(address), [selected]);
 
   const onCloseModal = useCallback(() => {
     setSelected([]);
+    setChatName('');
+    setError(false);
     onClose();
   }, [onClose]);
 
@@ -64,15 +72,21 @@ const CreateChatModal: React.FunctionComponent<Props> = ({ isOpen, onClose }) =>
     setIsLoading(false);
   }, [myAddress]);
 
-  const createNewChat = useCallback(
-    async (otherAccountAddress: string | undefined) => {
-      /* await API.graphql(
-        graphqlOperation(createConversation, { input: { account1ID: myAddress, account2ID: otherAccountAddress } }),
-      ); */
+  const onClickCreateChat = useCallback(async () => {
+    if (!chatName) {
+      setError(true);
+    } else {
+      const { data: createChatData } = (await API.graphql(
+        graphqlOperation(createChat, { input: { name: chatName } }),
+      )) as GraphQLResult<CreateChatMutation>;
+      const chatID = createChatData?.createChat?.id;
+      for (const accountID of selected) {
+        await API.graphql(graphqlOperation(createAccountChat, { input: { chatID, accountID } }));
+      }
+      await API.graphql(graphqlOperation(createAccountChat, { input: { chatID, accountID: myAddress } }));
       onCloseModal();
-    },
-    [myAddress, onCloseModal],
-  );
+    }
+  }, [chatName, myAddress, onCloseModal, selected]);
 
   useEffect(() => {
     if (isOpen) {
@@ -87,6 +101,14 @@ const CreateChatModal: React.FunctionComponent<Props> = ({ isOpen, onClose }) =>
         <ModalHeader>New chat</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
+          <Input
+            placeholder="Chat Name"
+            size="md"
+            mb="5"
+            value={chatName}
+            onChange={updateChatName}
+            isInvalid={error}
+          />
           {isLoading ? (
             <Center>
               <ClipLoader loading color="#1A2980" />
@@ -113,7 +135,7 @@ const CreateChatModal: React.FunctionComponent<Props> = ({ isOpen, onClose }) =>
           <Button colorScheme="blue" mr={3} onClick={onCloseModal}>
             Close
           </Button>
-          <Button variant="ghost" onClick={onCloseModal}>
+          <Button variant="ghost" onClick={onClickCreateChat}>
             Create
           </Button>
         </ModalFooter>
