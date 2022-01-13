ed3d52da222b7188a453ba8f3f2df357e5e9240c
davidivad96 <davidivad96@gmail.com>
2022-01-13 16:46:20 +0100

    Fix text input to bottom

--- src/components/CurrentChat.tsx
+++ src/components/CurrentChat.tsx
@@ -18,35 +18,34 @@ const CurrentChat: React.FunctionComponent<Props> = ({
   updateText,
   handleKeyPress,
   onClickSendMessage,
-}) => {
-  return (
-    <Flex flexDir="column-reverse" h="calc(100vh - 75px)" overflowY="scroll">
-      <InputGroup bg="#FFF" minH="45" borderColor="#2298B4">
-        <Input value={text} onChange={updateText} onKeyPress={handleKeyPress} />
-        <InputRightElement>
-          <IconButton
-            aria-label="Send Message"
-            icon={<ChevronRightIcon />}
-            size="md"
-            colorScheme="blue"
-            onClick={onClickSendMessage}
-          />
-        </InputRightElement>
-      </InputGroup>
-      <VStack p="4" w="full">
-        {messages.map((message) => (
-          <HStack
-            key={message.id}
-            alignSelf={message.sender?.address === myAddress ? 'flex-end' : 'flex-start'}
-            flexDir={message.sender?.address === myAddress ? 'row' : 'row-reverse'}
-          >
-            <Avatar src={message.sender?.avatarUrl} bg="transparent" mx="2" size="sm" />
-            <Tag p="3">{message.content}</Tag>
-          </HStack>
-        ))}
-      </VStack>
-    </Flex>
-  );
-};
+}) => (
+  <Flex flexDir="column-reverse" h="calc(100vh - 75px)" overflowY="scroll">
+    <InputGroup bg="#FFF" minH="45" borderColor="#2298B4" zIndex={1}>
+      <Input value={text} onChange={updateText} onKeyPress={handleKeyPress} pos="fixed" />
+      <InputRightElement>
+        <IconButton
+          aria-label="Send Message"
+          icon={<ChevronRightIcon />}
+          size="md"
+          colorScheme="blue"
+          onClick={onClickSendMessage}
+          pos="fixed"
+        />
+      </InputRightElement>
+    </InputGroup>
+    <VStack p="4" w="full">
+      {messages.map((message) => (
+        <HStack
+          key={message.id}
+          alignSelf={message.sender?.address === myAddress ? 'flex-end' : 'flex-start'}
+          flexDir={message.sender?.address === myAddress ? 'row' : 'row-reverse'}
+        >
+          <Avatar src={message.sender?.avatarUrl} bg="transparent" mx="2" size="sm" zIndex={0} />
+          <Tag p="3">{message.content}</Tag>
+        </HStack>
+      ))}
+    </VStack>
+  </Flex>
+);
 
 export default CurrentChat;
