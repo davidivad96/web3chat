389038ea157f4f7dedbe586132223b92caf98100
davidivad96 <davidivad96@gmail.com>
2022-01-27 19:26:30 +0100

    Add robot avatar to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { ConnectWallet } from '@3rdweb/react';
-import { Center, Heading, Text, VStack } from '@chakra-ui/react';
+import { Avatar, Center, Heading, Text, VStack } from '@chakra-ui/react';
 
 const Home: React.FunctionComponent = () => (
   <Center h="full" color="#FFF">
     <VStack spacing={5}>
       <VStack>
+        <Avatar size="2xl" src="https://avatars.dicebear.com/api/bottts/web3chat.svg" bg="transparent" />
         <Heading fontSize="6xl">Welcome to Web3Chat!</Heading>
         <Text fontSize="3xl">Connect your wallet to start chatting</Text>
       </VStack>
