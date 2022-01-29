249f93d331ae4dc7d6eee22402aab2e65913c5e2
davidivad96 <davidivad96@gmail.com>
2022-01-29 16:57:17 +0100

    Add text warning the user to install MetaMask to connect the wallet

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { ConnectWallet } from '@3rdweb/react';
-import { Avatar, Center, Heading, Text, VStack } from '@chakra-ui/react';
+import { Avatar, Center, Heading, Link, Text, VStack } from '@chakra-ui/react';
 
 const Home: React.FunctionComponent = () => (
   <Center h="full" color="#FFF">
@@ -8,6 +8,13 @@ const Home: React.FunctionComponent = () => (
         <Avatar size="2xl" src="https://avatars.dicebear.com/api/bottts/web3chat.svg" bg="transparent" />
         <Heading fontSize="6xl">Welcome to Web3Chat!</Heading>
         <Text fontSize="3xl">Connect your wallet to start chatting</Text>
+        <Text fontSize="xl">
+          (You need to install{' '}
+          <Link href="https://metamask.io/" isExternal>
+            MetaMask
+          </Link>{' '}
+          to do it)
+        </Text>
       </VStack>
       <ConnectWallet size="lg" colorScheme="blackAlpha" />
     </VStack>
